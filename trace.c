40824fa800b8e06576ac90f0e3b2b0555fdf09c7
bpisak-l <bpisak-l@student.42.fr>
2024-09-02 17:04:24 +0200

    Lil refactor

--- trace.c
+++ trace.c
@@ -6,7 +6,7 @@
 /*   By: bpisak-l <bpisak-l@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/02 11:59:07 by bpisak-l          #+#    #+#             */
-/*   Updated: 2024/09/02 16:45:03 by bpisak-l         ###   ########.fr       */
+/*   Updated: 2024/09/02 17:04:03 by bpisak-l         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -51,18 +51,35 @@ t_vec	ray_in_t(t_ray r, float t)
 	add(&res, r.r0);
 	return (res);
 }
+char	is_shadowed(t_light l, t_ray ray, float distance, int i)
+{
+	t_point	hit_point;
+	int		j;
+	float	t;
+	t_ray	ray2;
+
+	if (i == -1)
+		return (0);
+	ray2 = shape_to_light(distance, ray, l);
+	j = -1;
+	while (++j < state()->n_shapes)
+	{
+		t = ray_hit(state()->shapes[j], ray2);
+		if (j != i && !isnan(t))
+		{
+			hit_point = ray_in_t(ray2, t);
+			if (d_sq(l.position, ray2.r0) > d_sq(l.position, hit_point))
+				return (1);
+		}
+	}
+	return (0);
+}
 
 void	ray_color(t_ray *ray)
 {
 	float	*distances;
 	int		i;
-	t_ray	ray2;
-	int		j;
-	float	t;
-	t_point	hit_point;
-	t_light	light;
 
-	light = state()->lights[0];
 	i = -1;
 	distances = ft_calloc(state()->n_shapes, sizeof(float));
 	while (++i < state()->n_shapes)
@@ -72,29 +89,13 @@ void	ray_color(t_ray *ray)
 	i = minimum_distance(distances, state()->n_shapes);
 	if (i != -1)
 	{
-		ray2 = shape_to_light(distances[i], *ray, light);
-		j = -1;
-		while (++j < state()->n_shapes)
+		if (is_shadowed(state()->lights[0], *ray, distances[i], i))
+			ray->color = (t_color){.r = 0, .g = 0, .b = 0};
+		else
 		{
-			t = ray_hit(state()->shapes[j], ray2);
-			if (j != i && !isnan(t))
-			{
-				hit_point = ray_in_t(ray2, t);
-				if (d_sq(light.position, ray2.r0) > d_sq(light.position,
-						hit_point))
-				{
-					multiply_color(&ray->color, (t_color){.r = 0, .g = 0,
-							.b = 0});
-					return ;
-				}
-			}
+			multiply_color(&ray->color, state()->shapes[i]->color);
+			multiply_color(&ray->color, state()->lights[0].color);
 		}
-		multiply_color(&ray->color, state()->shapes[i]->color);
-		multiply_color(&ray->color, light.color);
-		free(distances);
-		return ;
 	}
 	free(distances);
-	ray->color = (t_color){.r = 0, .g = 0, .b = 0};
-	return ;
 }

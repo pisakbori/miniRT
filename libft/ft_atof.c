de53c083ad74ea3216376478f3fb601813a35ccf
cmakario <cmakario@student.42.fr>
2024-09-20 14:24:45 +0200

    scientific notation fixed, done

--- libft/ft_atof.c
+++ libft/ft_atof.c
@@ -6,7 +6,7 @@
 /*   By: cmakario <cmakario@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/17 17:38:42 by cmakario          #+#    #+#             */
-/*   Updated: 2024/09/20 07:12:38 by cmakario         ###   ########.fr       */
+/*   Updated: 2024/09/20 14:22:00 by cmakario         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -46,6 +46,33 @@ double	parse_fractional_part(const char **str)
 	return (result / fraction);
 }
 
+double	scientific_notation(const char **str)
+{
+	double	expo_sign;
+	double	expo;
+
+	expo_sign = 1.0;
+	expo = 0.0;
+	if (**str == 'e' || **str == 'E')
+	{
+		(*str)++;
+		if (**str == '-')
+		{
+			expo_sign = -1;
+			(*str)++;
+		}
+		else if (**str == '+')
+			(*str)++;
+	}
+	while (ft_isdigit(**str))
+	{
+		expo = expo * 10 + (**str - '0');
+		(*str)++;
+	}
+	printf("exposing * expo %f\n", expo_sign * expo);
+	return (expo_sign * expo);
+}
+
 double	ft_atof(const char *str)
 {
 	double	result;
@@ -53,7 +80,6 @@ double	ft_atof(const char *str)
 
 	result = 0;
 	sign = 1;
-
 	while (ft_is_space(*str))
 		str++;
 	if (*str == '-')
@@ -62,129 +88,19 @@ double	ft_atof(const char *str)
 		str++;
 	result = parse_interger_part(&str);
 	result += parse_fractional_part(&str);
+	result *= pow(10, scientific_notation(&str));
 	return (sign * result);
 }
 
-
-/* // Too big to make it smaller 
-double	ft_atof(const char *str)
-{
-	double	result;
-	double	fraction;
-	int		sign;
-
-	result = 0.0;
-	fraction = 1.0;
-	sign = 1;
-
-	while (ft_is_space(*str))
-		str++;
-	if (*str == '-')
-	{
-		sign = -1;
-		str++;
-	}
-	else if (*str == '+')
-		str++;
-	while (ft_isdigit(*str))
-	{
-		result = result * 10.0 + (*str - '0');
-		str++;
-	}
-	if (*str == '.')
-	{
-		str++;
-		while (ft_isdigit(*str))
-		{
-			result = result * 10.0 + (*str - '0');
-			fraction *= 10.0;
-			str++;
-		}
-	}
-	result = sign * result / fraction;
-	if (result == 0.0 && sign == -1)
-		return (-0.0);
-	return (result);
-} */
-
-
-
-int main() {
+/* ----------TEST-------------- */
+/* int main() {
+	printf("%f\n", ft_atof(" -123.456e-2"));	// Should print: -0.000000
+	printf("%f\n", ft_atof("14e2"));
+	printf("%f\n", atof("14e2"));
 	printf("%f\n", ft_atof("-0"));	// Should print: -0.000000
 	printf("%f\n", ft_atof("-0.0"));  // Should print: -0.000000
 	printf("%f\n", ft_atof("0.0"));   // Should print: 0.000000
 	printf("%f\n", ft_atof("123.456"));// Should print: 123.456000
 	printf("%f\n", ft_atof("-123.456"));// Should print: -123.456000
 	return 0;
-}
-
-// new idea with the ft atoi implementation with ft_atoi TOO BIG
-// double	ft_atof(const char *str)
-// {
-// 	int		int1;
-// 	int		frct2;
-// 	char	*prt1;
-// 	char	*prt2;
-// 	int		i;
-// 	double		p1;
-// 	double		p2;
-// 	double		final;
-
-// 	int1 = 0;
-// 	frct2 = 0;
-// 	i = 0;
-// 	while (str[int1] != '.' && str[int1] != '\0')
-// 		int1++;
-// 	if (str[int1] != '.')
-// 	{
-// 		// printf("Error: Decimal point '.' not found in input string.\n");
-// 		printf("Epistrefei %f\n", (double)ft_atoi(str));
-// 		return (ft_atoi(str));
-// 	}
-// 	prt1 = (char *)malloc(sizeof(char) * int1);
-// 	while (i < int1)
-// 	{
-// 		prt1[i] = str[i];
-// 		i++;
-// 	}
-// 	prt1[int1] = '\0';
-// 	p1 = ft_atoi(prt1);
-// 	printf("To prwto meros p1 %f\n", p1);
-// 	// free(prt1);
-// 	if (str[int1] == '.')
-// 	{
-// 		i = 0;
-// 		frct2 = ft_strlen(str) - int1 - 1;
-// 		prt2 = (char *)malloc(sizeof(char) * (frct2 + 1));
-// 		while (i < frct2)
-// 		{
-// 			prt2[i] = str[i + int1 + 1];
-// 			i++;
-// 		}
-// 		prt2[frct2] = '\0';
-// 		p2 = ft_atoi(prt2);
-// 		printf("To 2o merosss p2 %f\n", (double)p2/pow(10,frct2));
-// 		// free(prt2);
-// 	}
-// 	if (p1 >= 0)
-// 		final = p1 + p2 / pow(10, frct2);
-// 	else
-// 		final = p1 - p2 / pow(10, frct2);
-// 	printf("finale %f\n", final);
-// 	free(prt1);
-// 	free(prt2);
-
-// 	return (0);
-// }
-
-// int	main()
-// {
-// 	char	*a;
-
-// 	a = "-0";
-// 	ft_atof(a);
-// 	printf("------real atof------ %f\n", atof(a));
-// 	printf("... %d\n", ft_atoi(a));
-// 	printf("------real---- %d\n", atoi(a));
-// 	return (0);
-// }
+} */

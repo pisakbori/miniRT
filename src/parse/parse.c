017e77c8dce746a92bfb1f066f660c7c10a287a5
cmakario <cmakario@student.42.fr>
2024-09-20 18:58:52 +0200

    fixed .rt check

--- src/parse/parse.c
+++ src/parse/parse.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   parse.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: bpisak-l <bpisak-l@student.42.fr>          +#+  +:+       +#+        */
+/*   By: cmakario <cmakario@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/11 16:09:55 by bpisak-l          #+#    #+#             */
-/*   Updated: 2024/09/16 13:57:47 by bpisak-l         ###   ########.fr       */
+/*   Updated: 2024/09/20 18:53:36 by cmakario         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,6 +25,8 @@ void	parse_input(int argc, char **argv)
 
 	if (argc != 2)
 		exit_on_error("Correct usage: ./miniRT <image file>");
+	if (ft_strncmp((argv[1] + (ft_strlen(argv[1]) - 3)), ".rt", 3) != 0)
+		exit_on_error("Correct usage: ./miniRT <image file>.rt");
 	fd = open(argv[1], O_RDONLY);
 	if (fd == -1)
 		exit_on_error("Unable to open file.");
--- todo.md
+++ todo.md
@@ -7,7 +7,7 @@ clean 	float			brightness; in color
 
 
 Costas:
-check if extension is *.rt
-atof
+✅check if extension is *.rt
+✅atof
 Elements which are defined by a capital letter can only be declared once in
 the scene. -> Check if there's only 1 from all these in mandatory: C, A, L 
